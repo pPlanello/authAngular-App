aa92d95bb926e6e0b160d0891d8b644d16c850fd
pPlanello <planello96@gmail.com>
2022-01-24 12:03:48 +0100

    Creating forms in login and register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(4) ]],
+    email: ['', [Validators.required, Validators.email ]],
+    password: ['', [Validators.required, Validators.minLength(6) ]]
+  });
+
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.myForm.value);
+    this.router.navigateByUrl('./home');
   }
 
 }
